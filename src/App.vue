d77a6dab5403dd82b8f64e009369879fab9f4163
Ruben E Acosta <eldragondera1@gmail.com>
2019-03-05 13:26:24 -0500

    iniciando

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  #app
+    h1 {{titulo}}
+    p(v-for="(nombre, key) in nombres" v-bind:key="key") {{nombre}}
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      titulo: 'Hola Selene',
+      nombres: ['Tito', 'Pepe', 'Lilu']
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
