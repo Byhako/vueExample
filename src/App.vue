a32eced0073702cb397e8e5b6f9a73c62375e434
Ruben E Acosta <eldragondera1@gmail.com>
2019-03-12 11:29:41 -0500

    computed

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template lang="pug">
   #app
     h1 {{titulo}}
-    p(v-for="(nombre, key) in nombres" v-bind:key="key") {{nombre}}
-    input(type='text' v-model='nuevoNombre')
-    button(@click='addName') Agregar nombre
+    input(type='text' v-model='nuevoItem' @keyup.enter='addItem')
+    button(@click='addItem') Agregar item
+    ul
+      li(v-for="(item, key) in items" v-bind:key="key")
+        button(@click='item.cantidad++') +
+        button(@click='item.cantidad--') -
+        input(type='text' v-model.number='item.cantidad')
+        span {{ item.nombre }}
+        span(v-if='item.cantidad === 0') - Sin stock
+
+    h3 Total : {{sumarItems}}
+        
 </template>
 
 <script>
@@ -11,19 +20,41 @@ export default {
   name: 'app',
   data () {
     return {
-      titulo: 'Hola Selene',
-      nombres: ['Tito', 'Pepe', 'Lilu'],
-      nuevoNombre: ''
+      titulo: 'Lista de mercado',
+      items: [
+        {cantidad: 2, nombre: 'Tomate'},
+        {cantidad: 5, nombre: 'Pimienta'},
+        {cantidad: 0, nombre: 'Lulo'}
+      ],
+      nuevoItem: '',
+      total: 0
     }
   },
   methods: {
-    addName () {
-      this.nombres.push(this.nuevoNombre)
+    addItem () {
+      this.items.push({cantidad: 1, nombre: this.nuevoItem})
+      this.nuevoItem = ''
+    }
+  },
+  computed: {
+    sumarItems () {
+      this.total = 0
+      this.items.forEach(item => {
+        this.total = this.total + item.cantidad
+      })
+      return this.total
     }
   }
 }
 </script>
 
 <style lang="scss">
-
+  li {
+    margin-bottom: 5px;
+  }
+  li input {
+    width: 25px;
+    text-align: center;
+    margin: 0 10px;
+  }
 </style>
